7e5a4deaf04f37ace273c438b8e6effb6d174e2f
TÜRKAY BİLİYOR <turkaybiliyor@hotmail.com>
2017-11-14 21:48:47 +0300

    Update main.cpp

--- BalanceRobotPi/main.cpp
+++ BalanceRobotPi/main.cpp
@@ -34,7 +34,8 @@
 
 #define SLEEP_PERIOD 1000 //us;
 #define SERIAL_TIME 100 //ms
-#define SAMPLE_TIME 9 //ms
+#define SAMPLE_TIME 10 //ms
+int pwnLimit = 255;
 
 #define KF_VAR_ACCEL 0.0075 // Variance of pressure acceleration noise input.
 #define KF_VAR_MEASUREMENT 0.05
@@ -74,7 +75,6 @@ int Turn_Need = 0;
 int Speed_Diff = 0;
 int Speed_Diff_ALL = 0;
 int Position_AVG = 0;
-int pwnLimit = 255;
 
 uint32_t timer;
 int16_t ax, ay, az;
@@ -85,7 +85,7 @@ double timediff = 0.0;
 double Correction = 0.0;
 double Setpoint = 0.0;
 double aggKp = 40.0;
-double aggKi = 190.0;
+double aggKi = 75.0;
 double aggKd = 1.8;
 double aggVs = 10.0; //Velocity wheel
 double aggKm = 1.0; //Velocity wheel
@@ -642,11 +642,7 @@ void calculateGyro()
 
 }
 
-
-/*
-alternate pid calculation
-
-float K = 1.064 ;       //1.9 * 1.12;  // wheels 80mm
+/*float K = 1.064 ;       //1.9 * 1.12;  // wheels 80mm
 //float K = 1.9 ;      // wheels 100mm
 float Kp = 15;
 float Ki = 6.9;
@@ -659,18 +655,18 @@ int integrated_error = 0;
 float pTerm=0, iTerm=0, dTerm=0, pTerm_Wheel=0, dTerm_Wheel=0;
 
 int updatePid(float targetPosition, float currentPosition)   {
-  float error = targetPosition - currentPosition;
-  pTerm = Kp * error;
-  integrated_error += error;
-  iTerm = Ki * constrain(integrated_error, -GUARD_GAIN, GUARD_GAIN);
-  dTerm = Kd * (error - last_error);
-  last_error = error;
-  pTerm_Wheel = Kp_Wheel * count;           //  -(Kxp/100) * count;
-  dTerm_Wheel = Kd_Wheel * (count - last_count);
-  last_count = count;
-  return -constrain(K*(pTerm + iTerm + dTerm + pTerm_Wheel + dTerm_Wheel), -255, 255);
-}
+    float error = targetPosition - currentPosition;
+    pTerm = Kp * error;
+    integrated_error += error;
+    iTerm = Ki * constrain(integrated_error, -GUARD_GAIN, GUARD_GAIN);
+    dTerm = Kd * (error - last_error);
+    last_error = error;
+    pTerm_Wheel = Kp_Wheel * count;           //  -(Kxp/100) * count;
+    dTerm_Wheel = Kd_Wheel * (count - last_count);
+    last_count = count;
+    return -constrain(K*(pTerm + iTerm + dTerm + pTerm_Wheel + dTerm_Wheel), -255, 255);
 }*/
+
 
 void encodeL (void)
 {
